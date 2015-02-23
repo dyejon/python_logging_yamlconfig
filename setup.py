a221089bfea84bde98aaed906c7321335bb41cc7
Jonathan Dye <jdye@us.ibm.com>
2015-02-23 14:57:37 -0700

    added urls

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ import os
 from setuptools import setup, find_packages, findall
 
 def get_version():
-	with open(os.path.join(thisdir, 'VERSION'), 'r') as fh:
-		return fh.readline().strip()
+    with open(os.path.join(thisdir, 'VERSION'), 'r') as fh:
+        return fh.readline().strip()
 
 def get_requires(filename='requirements.txt'):
-	requires = []
-	with open(os.path.join(thisdir, filename), 'r') as fh:
-		for line in fh.readlines():
-			requires += [ line.strip() ]
-	return requires
+    requires = []
+    with open(os.path.join(thisdir, filename), 'r') as fh:
+        for line in fh.readlines():
+            requires += [ line.strip() ]
+    return requires
 
 thisdir = os.path.dirname(os.path.realpath(__file__))
 
@@ -28,16 +28,18 @@ ALL_TEST_REQUIRES = get_requires('test_requirements.txt')
 ALL_SETUP_REQUIRES = get_requires('setup_requirements.txt')
 
 setup(
-	name='logging_yamlconfig',
-	version=VERSION,
-	description='Python dictconfig helpers for yaml config files',
-	author='IBM',
-	author_email='jdye@us.ibm.com',
-	packages=ALL_PACKAGES,
-	provides=ALL_PACKAGES,
-	scripts=SCRIPTS,
-	include_package_data=True,
-	setup_requires=ALL_SETUP_REQUIRES,
+    name='logging_yamlconfig',
+    version=VERSION,
+    description='Python dictconfig helpers for yaml config files',
+    author='IBM',
+    author_email='jdye@us.ibm.com',
+    url='https://github.com/dyejon/python_logging_yamlconfig',
+    download_url='https://github.com/dyejon/python_logging_yamlconfig/tarball/%s' % VERSION,
+    packages=ALL_PACKAGES,
+    provides=ALL_PACKAGES,
+    scripts=SCRIPTS,
+    include_package_data=True,
+    setup_requires=ALL_SETUP_REQUIRES,
     install_requires=ALL_INSTALL_REQUIRES,
     tests_require=ALL_TEST_REQUIRES,
     test_suite='nose.collector'
